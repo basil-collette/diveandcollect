806fa8509267d0cd4317f2cfe407f4399e1e23c9
Lucien <lucien.zak@laplateforme.io>
2023-06-01 15:21:27 +0200

    ✨Importe et utilise la librairie TFJS et logique de reconaissance d'image

--- frontend/React app - theme Vristo/src/pages/Pages/Identification/Identification.tsx
+++ frontend/React app - theme Vristo/src/pages/Pages/Identification/Identification.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import AnimateHeight from 'react-animate-height';
 import { setPageTitle } from '../../../store/themeConfigSlice';
 import { useDispatch } from 'react-redux';
+import * as tf from '@tensorflow-models/mobilenet';
+import '@tensorflow/tfjs-backend-webgl';
 
 const Identification = () => {
     const dispatch = useDispatch();
@@ -16,9 +18,37 @@ const Identification = () => {
         });
     };
 
+    const prediction = () => {
+        tf.load().then((model) => {
+            // do something with the model
+            const img = document.querySelector('img') as HTMLImageElement;
+            console.log(img);
+            model.classify(img).then((predictions) => {
+                document.querySelector('span').innerHTML = predictions[0].className + ' ' + predictions[0].probability.toFixed(2);
+            });
+        });
+    };
+
+
+    const handleUploadImage = (file) => {
+        document.querySelector('img').src = URL.createObjectURL(file);
+        prediction();
+    };
+
+
+
+
     return (
         <div className="container">
             <h1>Hello BASIL</h1>
+            <form>
+            <input
+          type="file"
+        //   value={selectedFile}
+          onChange={(e) => handleUploadImage(e.target.files[0])}
+        />            </form>
+            <img src='/public/assets/images/carousel1.jpeg'/>
+            <span></span>
         </div>
     );
 };
