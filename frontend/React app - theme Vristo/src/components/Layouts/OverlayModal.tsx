831f8fe8bb1dfd53a8f159bac899b7c4ed5679a8
basil-collette <basil.collette@laplateforme.io>
2023-06-02 13:48:55 +0200

    identification basil

--- frontend/React app - theme Vristo/src/components/Layouts/OverlayModal.tsx
+++ frontend/React app - theme Vristo/src/components/Layouts/OverlayModal.tsx
@@ -1,21 +1,10 @@
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useLocation } from 'react-router-dom';
-import { toggleSidebar } from '../../store/themeConfigSlice';
-import AnimateHeight from 'react-animate-height';
-import { IRootState } from '../../store';
-import { PropsWithChildren, useState, useEffect } from 'react';
-
 const OverlayModal = (props: any) => {
 
-    
-
     return (
-        <div className="absolute w-full h-full top-0 left-0 bg-black bg-opacity-70 p-16">
-            <div className="relative text-xl w-full h-full flex flex-col bg-lagon rounded-xl bg-opacity-80 transition-all">
+        <div className="absolute w-full h-full top-0 left-0 bg-black bg-opacity-70 p-16" style={{zIndex: 400}}>
+            <div className="relative text-xl w-full h-full flex flex-col bg-lagon text-outremer rounded-xl shadow-2xl p-16">
                 
-                <span className="absolute top-8 right-8 text-white cursor-pointer"
+                <span className="absolute top-8 right-8 text-white cursor-pointer hover:text-fushia"
                     onClick={() => {props.hideModal()}}
                     >
                     X
--- frontend/React app - theme Vristo/src/pages/Identification/Identification.tsx
+++ frontend/React app - theme Vristo/src/pages/Identification/Identification.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import AnimateHeight from 'react-animate-height';
 import { setPageTitle } from '../../store/themeConfigSlice';
 import { useDispatch } from 'react-redux';
@@ -11,33 +11,38 @@ const Identification = () => {
 
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(setPageTitle('Knowledge Base'));
+        dispatch(setPageTitle('Identification'));
     });
 
-    const [showModal, setShowModal] = useState<boolean>(true);
+    const uploadedImgRef = useRef(null)
 
-    let file: any = null;
+    const [state, setState] = useState<{showModal:boolean, uploadedImg: File | null, prediction: any}>({
+        showModal: false,
+        uploadedImg: null,
+        prediction: null
+    });
 
-    const prediction = async (img: HTMLImageElement) => {
+    useEffect(() => {
+        if(uploadedImgRef.current) {
+            prediction();
+        }
+    }, [state.showModal]);
+
+    const prediction = async () => {
+        const img = uploadedImgRef.current as unknown as HTMLImageElement;
+        
+        img.src = URL.createObjectURL(state.uploadedImg!);
+
         const model = await tf.load();
 
         const predictions = await model.classify(img);
-        
-        const result: string = predictions[0].className + ' ' + predictions[0].probability.toFixed(2);
-        console.log(result);
-        
-        //document.querySelector('span').innerHTML = result;
+        //console.log(predictions[0].probability.toFixed(2));
+
+        setState((prevState) => {return({
+            ...prevState,
+            prediction: predictions[0]
+        });});
     };
-
-    const showModalProcess = () => {
-
-        const img = document.querySelector('#uploadedImg') as HTMLImageElement;
-        img.src = URL.createObjectURL(file);
-
-        prediction(img);
-
-        //
-    }
 
     return (
         <div className="w-full h-full bg-blue-grey flex flex-col justify-around items-center p-40 text-outremer">
@@ -45,25 +50,61 @@ const Identification = () => {
 
             <p className="text-lg text-center">Lorem ipsum dolor sit amet, consetetur sadipsing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
             
-            <label className="relative text-white font-bold text-2xl rounded-full px-12 py-4 bg-outremer">
+            <label className="relative text-white font-bold text-2xl rounded-full px-12 py-4 bg-outremer cursor-pointer">
                 Selectionner votre fichier
 
-                <input type="file"
+                <input type="file" multiple={false}
                     className="absolute opacity-0 w-full h-full left-0 top-0 cursor-pointer"
                     onChange={(e) => {
-                        file = e.target.files[0];
-                        showModalProcess();
+                        setState((prevState) => { return ({
+                            ...prevState,
+                            showModal: true,
+                            uploadedImg: e.target.files![0]
+                        });});
                     }}
                 />
             </label>
 
             <span className="-mt-16 text-lg">ou déposer l'image ici</span>
         
-            { showModal &&
-                <OverlayModal hideModal={() => {setShowModal(false)}} >
+            { state.showModal &&
+                <OverlayModal hideModal={() => {
+                    setState((prevState) => { return ({
+                        ...prevState,
+                        showModal: true
+                    });});}}
+                    >
                     
-                    <div className="text-white flex flex-column">
-                        <p>TEST</p>
+                    <div className="h-full w-full flex flex-col justify-between items-center relative">
+                        <div className="w-full h-1/2 flex flex-row justify-center">
+                            <img ref={uploadedImgRef} id="uploadedImg" src="" className="h-full object-contain rounded-xl" />
+                            
+                            <div className="h-full flex flex-col justify-center items-center gap-8 p-8">
+                                { state.prediction == null
+                                    ?   <>
+                                            <p className="text-white">Identification...</p>
+                                            <img className="w-16 h-16" src="../../../public/assets/images/spinner.gif" alt="loading spinner" />
+                                        </>
+                                    :   <>
+                                            <p className="text-white">Identification réussie !</p>
+                                            <p className="uppercase font-bold text-4xl">{state.prediction.className}</p>
+                                        </>
+                                }
+                            </div>
+                        </div>
+
+                        <div className="h-1/2 w-full flex flex-col justify-between items-center relative">
+                            <p className="w-full h-full flex flex">test</p>
+
+                            <div className="w-full flex flex-row justify-end items-center gap-16 text-white">
+                                <Link to="/" className="rounded-full px-6 py-2 bg-fushia hover:underline ltr:ml-1 rtl:mr-1">
+                                    Envoyer
+                                </Link>
+                                <Link to="/" className="text-fushia font-bold hover:underline ltr:ml-1 rtl:mr-1">
+                                    Télécharger
+                                </Link>
+                            </div>
+                        </div>             
                     </div>
 
                 </OverlayModal>
